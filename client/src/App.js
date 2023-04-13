fbdfb6f1f74170c03e9e5c3d680191766c8ca73d
Shivani9314 <guptashivani93143@gmail.com>
2023-04-13 13:48:27 +0530

    sd

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home/Home';
+import Categories from './pages/categories/Categories';
+import ProductDetails from './pages/productDetails/ProductDetails';
+import Navbar from './component/navbar/Navbar'
+import Footer from './component/footer/Footer'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        hello
-      </header>
-    </div>
-  );
+
+    
+    return (
+        <div className="App">
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/products' element={<Categories/>}/>
+                <Route path='/products/:productId' element={<ProductDetails/>}/>
+
+            </Routes>
+            <Footer/>
+            
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>Homesdf</div>
+    )
+}
+
+export default Home
